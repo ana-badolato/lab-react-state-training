c9cca99be07dc35d6f2190631124540e2658ac3b
Ana Badolato <badolatoana@gmail.com>
2024-09-18 23:20:57 +0200

    Iteration 1

---
+++ src/components/LikeButton.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function LikeButton() {
+  // Initialize likes state with 0
+  const [likes, setLikes] = useState(0);
+
+  const handleLikes = () => {setLikes(likes+1)}
+
+  return (
+    <div>
+      <button onClick={handleLikes}>{likes} Likes</button>
+    </div>
+  )
+}
+
+export default LikeButton
